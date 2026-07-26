e6b22f19c3547dbf83c8315c132b64ada2492cc2
ordinaryCoder <rivetrax@gmail.com>
2026-07-26 15:25:39 +0530

    build error fix

--- apps/web/app/confirm-email/_components/ConfirmEmailContent.tsx
+++ apps/web/app/confirm-email/_components/ConfirmEmailContent.tsx
@@ -4,7 +4,7 @@ import { AcademyLogo } from "@/components/icons";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState, useTransition, useEffect } from "react";
 import { resendConfirmationEmail } from "../_actions/action";
-import { signOut } from "../_actions/action";
+import { signOut } from "@/app/actions";
 
 export default function ConfirmEmailContent() {
   const searchParams = useSearchParams();
